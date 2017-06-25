d104fc1f531bb92d5d4a54393039b8deef0dee91
gyh75520 <734468130@qq.com>
2017-06-26 00:14:17 +0800

    Add files via upload

---
+++ clearby12.py
@@ -0,0 +1,15 @@
+from tornado import ioloop, gen
+import tornado_mysql
+from tornado.escape import json_encode,json_decode
+
+@gen.coroutine
+def main():
+    conn =yield tornado_mysql.connect(host="127.0.0.1", user="root", passwd="root", db="bangzai", port=3306)
+    cur = conn.cursor()
+    yield cur.execute("update food set sales=0")
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+ioloop.IOLoop.current().run_sync(main)
+
